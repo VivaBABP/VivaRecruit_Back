e37c21ae901a5f7e28c02d40395c2f55a7888998
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2023-04-23 23:00:47 +0200

    fusion en un seul fichier sql

--- prisma/migrations/20230423210032_bdd/migration.sql
+++ prisma/migrations/20230423210032_bdd/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `User`;
-
 -- CreateTable
 CREATE TABLE `Account` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
@@ -18,7 +9,8 @@ CREATE TABLE `Account` (
     `lastDiploma` VARCHAR(191) NOT NULL,
     `hr` BOOLEAN NOT NULL,
     `cv` BLOB NOT NULL,
-    `profileTypeId` INTEGER NOT NULL,
+    `profileTypeId` INTEGER NULL,
+    `jobDescriptionId` INTEGER NULL,
 
     UNIQUE INDEX `Account_email_key`(`email`),
     UNIQUE INDEX `Account_phoneNumber_key`(`phoneNumber`),
@@ -49,6 +41,7 @@ CREATE TABLE `Interests` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `labelInterest` VARCHAR(191) NOT NULL,
     `accountId` INTEGER NULL,
+    `panelId` INTEGER NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -62,7 +55,13 @@ CREATE TABLE `Panel` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Account` ADD CONSTRAINT `Account_profileTypeId_fkey` FOREIGN KEY (`profileTypeId`) REFERENCES `ProfileType`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Account` ADD CONSTRAINT `Account_profileTypeId_fkey` FOREIGN KEY (`profileTypeId`) REFERENCES `ProfileType`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Account` ADD CONSTRAINT `Account_jobDescriptionId_fkey` FOREIGN KEY (`jobDescriptionId`) REFERENCES `JobDescription`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Interests` ADD CONSTRAINT `Interests_accountId_fkey` FOREIGN KEY (`accountId`) REFERENCES `Account`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Interests` ADD CONSTRAINT `Interests_panelId_fkey` FOREIGN KEY (`panelId`) REFERENCES `Panel`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
