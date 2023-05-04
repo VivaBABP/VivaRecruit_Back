3e8d87625a707a3880bff8bb34ca53dd3022d944
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2023-05-04 13:51:09 +0200

    fix

--- src/auth/dto/validation-code.dto.ts
+++ src/auth/dto/validation-code.dto.ts
@@ -1,13 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, Max, Min } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class ValidationCodeDTO {
   @ApiProperty()
   @IsNotEmpty()
-  idUser: number;
+  email: string;
   @ApiProperty()
   @IsNotEmpty()
-  @Min(1)
-  @Max(4)
   code: number;
 }
