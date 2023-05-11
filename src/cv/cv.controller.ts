63ccc3aa1fb06539ba58ad3fd6141c4bd00822bd
JulienBois <71647803+JulienBois@users.noreply.github.com>
2023-05-11 18:49:24 +0200

    Utilisation du token pour l'upload du cv et changement de type pour la colonne cv pour pouvoir upload de plus gros fichiers (#16)

--- src/cv/cv.controller.ts
+++ src/cv/cv.controller.ts
@@ -1,6 +1,7 @@
 import {
   Controller,
   Post,
+  Req,
   UploadedFile,
   UseGuards,
   UseInterceptors,
@@ -9,17 +10,43 @@ import { CvService } from './cv.service';
 import { Express } from 'express';
 import { JwtGuard } from '../jwt/guards/jwt.guard';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { TokenPayload } from '../interfaces/token-payload.interface';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiForbiddenResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Cv')
 @ApiBearerAuth()
 @Controller('cv')
 export class CvController {
   constructor(private readonly cvService: CvService) {}
 
   @UseGuards(JwtGuard)
+  @ApiForbiddenResponse()
+  @ApiOkResponse({ description: 'Fichier uploadé avec succès' })
   @Post()
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'file',
+          format: 'binary',
+        },
+      },
+    },
+  })
   @UseInterceptors(FileInterceptor('file'))
-  async uploadCv(@UploadedFile() file: Express.Multer.File) {
-    await this.cvService.uploadCv(file, 2); //Mise du cv dans un user spécifique car l'on ne récupère pas les données du token
+  async uploadCv(
+    @UploadedFile() file: Express.Multer.File,
+    @Req() req: { user: TokenPayload },
+  ): Promise<void> {
+    await this.cvService.uploadCv(file, req.user.sub);
   }
 }
--- src/cv/cv.service.ts
+++ src/cv/cv.service.ts
@@ -1,16 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { JwtService } from '@nestjs/jwt';
 import { Express } from 'express';
 
 @Injectable()
 export class CvService {
-  constructor(
-    private readonly prisma: PrismaService,
-    private readonly jwtService: JwtService,
-  ) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   async uploadCv(file: Express.Multer.File, id: number): Promise<void> {
+    if (file.mimetype != 'application/pdf') {
+      throw new ForbiddenException('Format du fichier incorrect');
+    }
+    if (file.size > 20000000) {
+      throw new ForbiddenException('Fichier trop lourd');
+    }
     await this.prisma.account.update({
       data: {
         cv: file.buffer,
