0c366903f4a9950a4b433d59299b49f4cade39b4
JulienBois <71647803+JulienBois@users.noreply.github.com>
2023-05-11 16:54:02 +0200

    Ajout de l'upload de fichier sur swagger et des exceptions sur le type des fichiers upload et leur tailles et npm run format

--- src/cv/cv.controller.ts
+++ src/cv/cv.controller.ts
@@ -1,6 +1,7 @@
 import {
   Controller,
-  Post, Req,
+  Post,
+  Req,
   UploadedFile,
   UseGuards,
   UseInterceptors,
@@ -10,19 +11,39 @@ import { Express } from 'express';
 import { JwtGuard } from '../jwt/guards/jwt.guard';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { TokenPayload } from '../interfaces/token-payload.interface';
-import {ApiTags} from "@nestjs/swagger";
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiForbiddenResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @ApiTags('Cv')
+@ApiBearerAuth()
 @Controller('cv')
 export class CvController {
   constructor(private readonly cvService: CvService) {}
 
   @UseGuards(JwtGuard)
+  @ApiForbiddenResponse()
   @Post()
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'file',
+          format: 'binary',
+        },
+      },
+    },
+  })
   @UseInterceptors(FileInterceptor('file'))
   async uploadCv(
-      @UploadedFile() file: Express.Multer.File,
-      @Req() req: { user: TokenPayload },
+    @UploadedFile() file: Express.Multer.File,
+    @Req() req: { user: TokenPayload },
   ) {
     console.log(req);
     await this.cvService.uploadCv(file, req.user.sub); //Mise du cv dans un user spécifique car l'on ne récupère pas les données du token
--- src/cv/cv.service.ts
+++ src/cv/cv.service.ts
@@ -16,6 +16,12 @@ export class CvService {
   ) {}
 
   async uploadCv(file: Express.Multer.File, id: number): Promise<void> {
+    if (file.mimetype != 'application/pdf') {
+      throw new ForbiddenException('Format du fichier incorrect');
+    }
+    if (file.size > 20000000) {
+      throw new ForbiddenException('Fichier trop lourd');
+    }
     await this.prisma.account.update({
       data: {
         cv: file.buffer,
